71767a839af28192b71d83c7033c119e35601aec
ikalafe <imkalafe@gmail.com>
2025-04-10 14:12:26 -0700

    feat: add username and bio fields to User model with validation and formatting

--- models/user.model.js
+++ models/user.model.js
@@ -17,6 +17,27 @@ const User = sequelize.define("User", {
   email: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
+    require: true,
+    set(value) {
+      this.setDataValue("email", value ? value.toLowerCase() : "");
+    },
+    validate: {
+      isEmail: true,
+    },
+  },
+  username: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+    require: true,
+    set(value) {
+      this.setDataValue("username", value ? value.toLowerCase() : "");
+    },
+  },
+  bio: {
+    type: DataTypes.STRING,
+    allowNull: true,
   },
 });
 
